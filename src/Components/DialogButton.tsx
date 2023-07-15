b5c0831d68e0f254ff271c2e9797cc120163b2d8
rladmstn <oz33177351@gmail.com>
2023-07-16 02:16:09 +0900

    design : Rank DialogButton 디자인 수정

--- src/Components/DialogButton.tsx
+++ src/Components/DialogButton.tsx
@@ -1,21 +1,21 @@
 // 랭킹 display 버튼
-
 import { useState, useEffect } from "react";
 import styled from "styled-components";
 
 const DialogButtonWrapper = styled.button`
-  width: 160px;
-  height: 40px;
-  background-color: gray;
+  width: 50vh;
+  height: cover;
+  background-color: white;
   color: black;
   font-size: 1.2rem;
   font-weight: 400;
   border-radius: 4px;
-  border: none;
+  border: 2px solid #B0E0E6;
   cursor: pointer;
+  transition: background-color 0.3s ease-in-out;
 
   &:hover {
-    transform: translateY(-1px);
+    background-color: #B0E0E6;
   }
 `;
 
@@ -27,16 +27,19 @@ interface DialogButtonProps {
 // DialogButton 컴포넌트
 const DialogButton: React.FC<DialogButtonProps> = ({ onClickToggleModal }) => {
     const [currentLetter, setCurrentLetter] = useState<string>('');
-    
+    const [currentRank, setCurrentRank] = useState(0);
+
+    let currentIndex = 0;
+
     useEffect(() => {
-        const alphabet = 'abcdefghijklmnopqrstuvwxyz'; // 랭킹 메뉴 및 가게명으로 대체될 예정
-        let currentIndex = 0;
+        const ranking = ['apple', 'banana', 'pear', 'cherry', 'strawberry', 'mellon']; // 랭킹 메뉴 및 가게명으로 대체될 예정
 
         const interval = setInterval(() => {
-            setCurrentLetter(alphabet[currentIndex]);
+            setCurrentLetter(ranking[currentIndex]);
+            setCurrentRank(currentIndex+1);
             currentIndex++;
 
-            if (currentIndex >= alphabet.length) {
+            if (currentIndex >= ranking.length) {
                 //clearInterval(interval); -> 1회 순환에서 끝남
                 currentIndex = 0;
             }
@@ -49,7 +52,7 @@ const DialogButton: React.FC<DialogButtonProps> = ({ onClickToggleModal }) => {
 
     return (
         <DialogButtonWrapper onClick={onClickToggleModal}>
-            <h1>{currentLetter}</h1>
+            <h1>{currentRank}. {currentLetter}</h1>
         </DialogButtonWrapper>
     );
 };
--- src/Components/Modal.tsx
+++ src/Components/Modal.tsx
@@ -11,6 +11,7 @@ const ModalContainer = styled.div`
   align-items: center;
   justify-content: center;
   position: fixed;
+  z-index: 9999;
 `;
 
 const DialogBox = styled.dialog`
